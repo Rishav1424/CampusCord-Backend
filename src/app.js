307683cd6de836063af6d95602854479c5688795
Rishav Sharma <sharmarishav676@gmail.com>
2025-07-30 02:43:30 +0530

    updated cors

--- src/app.js
+++ src/app.js
@@ -3,7 +3,7 @@ import cors from 'cors';
 import routes from './routes/index.js'; 
 
 const app = express();
-app.use(cors());
+app.use(cors({ origin: "*" }));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
